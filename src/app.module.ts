6d0965def8f816553769a8656eb6c84ecb94c47f
羽馬直樹 <haba@mic9.co.jp>
2022-04-22 04:18:45 +0900

    :sparkles: TypeORM設定読み込み

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      // 他のモジュールでConfigModuleをインポートせずに利用できるように
+      isGlobal: true,
+
+      // 環境変数を定義したファイルをインポート
+      load: [configuration],
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: Number(configService.get('database.port')),
+        username: configService.get('database.user'),
+        password: configService.get('database.pass'),
+        database: configService.get('database.name'),
+        entities: [join(__dirname, '**/entities/*.entity.{ts,js}')],
+        synchronize: false,
+        logging: configService.get('nodeEnv') === 'development',
+        extra: {},
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
