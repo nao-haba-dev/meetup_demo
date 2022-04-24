dc6672dee61f6827ff26856261a28e8aa86c2dc4
羽馬直樹 <haba@mic9.co.jp>
2022-04-25 02:40:52 +0900

    :sparkles: Validation設定

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // クロスオリジンリソース共有を有効にする
+  app.enableCors();
+
+  // バリデーションを自動で適用
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  process.exit(1);
+});
