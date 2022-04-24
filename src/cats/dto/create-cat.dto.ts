2b3d442ab7a6798a470310af28f973b7528c19d1
羽馬直樹 <haba@mic9.co.jp>
2022-04-25 01:38:29 +0900

    :sparkles: Dtoクラス実装

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,1 +1,7 @@
-export class CreateCatDto {}
+import { IsNotEmpty, MaxLength } from 'class-validator';
+
+export class CreateCatDto {
+  @IsNotEmpty({ message: '名前は必須項目です' })
+  @MaxLength(255, { message: '名前は255文字以内で入力してください' })
+  name: string;
+}
