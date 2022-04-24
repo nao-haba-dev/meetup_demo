91c68240c3e88f6e48ab9a49436863e2b911644a
羽馬直樹 <haba@mic9.co.jp>
2022-04-25 01:37:31 +0900

    :memo: コメント追加

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cat } from './entities/cat.entity';
 
 @Module({
+  // 特定のEntityのみimportします
+  imports: [TypeOrmModule.forFeature([Cat])],
+  // モジュール外でも利用できるようにします
+  exports: [TypeOrmModule],
   controllers: [CatsController],
   providers: [CatsService],
 })
